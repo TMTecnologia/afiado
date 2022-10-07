f412baf983e737f0853bd65c9242af9300c74673
Thales Menezes <thlmenezes@pm.me>
2022-10-07 00:56:08 -0300

    ✨ add to shopping cart behavior

--- sales-client/src/pages/index.tsx
+++ sales-client/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 
 type Produto = {
   id: string;
@@ -21,6 +22,8 @@ async function getProdutos(){
 
 const Home: NextPage = () => {
   const query = useQuery<{data: Produto[]}| undefined>(['produtos'], getProdutos);
+  const [increment, setIncrement] = useState(1);
+  const [shoppingCart, setShoppingCart] = useState({} as {[nome: string]: {quantity: number; price: number}});
 
   return (
     <>
@@ -31,22 +34,34 @@ const Home: NextPage = () => {
       </Head>
       <main className="container flex flex-col items-center justify-center min-h-screen p-4 mx-auto">
         <ul className="grid gap-5 md:gap-5 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-          {query.data?.data.map(produto => (
-            <li key={produto.id}>
-              <button className={`btn w-full h-40 p-auto btn-lg shadow-xl ${produto.attributes.quantidade > 0 ? '' : 'btn-disabled'}`}>
+          {query.data?.data.map(produto => {
+            function handleAddToCart(name: string, price: number){
+              return () => setShoppingCart((old) => {
+                const quantity = old[name]?.quantity ?? 0;
+                return {...old, [name]: {quantity: quantity + increment, price: price}}
+              })
+            }
+            
+            return (<li key={produto.id}>
+              <button
+                className={`btn w-full h-40 p-auto btn-lg shadow-xl ${produto.attributes.quantidade > 0 ? '' : 'btn-disabled'}`}
+                onClick={handleAddToCart(produto.attributes.nome, produto.attributes.preco)}
+              >
                 {produto.attributes.nome}
               </button>
-            </li>
-          ))}
+            </li>)
+          })}
         </ul>
         <div className="btn-group">
-          <button className="btn">+1</button>
-          <button className="btn">+2</button>
-          <button className="btn">+5</button>
+          <button className={`btn ${increment === 1 && 'btn-accent'}`} onClick={() => setIncrement(1)}>+1</button>
+          <button className={`btn ${increment === 2 && 'btn-accent'}`} onClick={() => setIncrement(2)}>+2</button>
+          <button className={`btn ${increment === 5 && 'btn-accent'}`} onClick={() => setIncrement(5)}>+5</button>
         </div>
         <button className="btn btn-outline btn-block">
           Carrinho
-          <div className="badge badge-secondary rounded-full">+99</div>
+          <div className="badge badge-secondary rounded-full">+{
+            Object.values(shoppingCart).reduce((acc, curr) => acc + curr.quantity,0)
+          }</div>
         </button>
       </main>
     </>
