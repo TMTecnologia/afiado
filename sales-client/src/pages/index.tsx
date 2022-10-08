a7644f2756aef870d645088bf50468759de6ff11
Thales Menezes <thlmenezes@pm.me>
2022-10-08 00:47:54 -0300

    💄 improved usability

--- sales-client/src/pages/index.tsx
+++ sales-client/src/pages/index.tsx
@@ -55,7 +55,14 @@ const Home: NextPage = () => {
   }
 
   function userAvatar({data}: dataType) {
-    if (!data) return <span className="text-xl">AA</span>;
+    if (!data)
+      return (
+        <div className="avatar placeholder mr-8">
+          <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
+            <span className="text-xl text-bold">AA</span>
+          </div>
+        </div>
+      )
 
     return (
       <div className="flex flex-col align-items justify-center">
@@ -75,11 +82,11 @@ const Home: NextPage = () => {
 
   function Navbar({data}: {data: dataType}){
     return (
-    <nav className="flex gap-2 py-10 min-w-full justify-end">
+    <nav className="flex gap-2 py-10 min-w-full justify-end mr-16">
       {
       isAuthenticaded(data) 
-        ? <button onClick={() => signOut()}>Logout</button>
-        : <button onClick={() => signIn('google')}>Login</button>
+        ? <button className="btn text-xl" onClick={() => signOut()}>SAIR</button>
+        : <button className="btn text-xl" onClick={() => signIn('google')}>LOGAR</button>
       }
       {userAvatar(data)}
     </nav>
@@ -96,16 +103,31 @@ const Home: NextPage = () => {
     setShoppingCart({} as ShoppingCart)
   }
 
+  function CartCount(){
+    const count = Object.values(shoppingCart)
+                        .reduce((acc, curr) => {
+                          return acc + curr.quantity
+                        },0)
+    if (count <= 0) return <></>
+    return (
+      <span className="badge badge-secondary rounded-full ml-2">
+        +{count}
+      </span>
+    )
+  }
+
   if(showCart){
     return (
-      <>
+      <div className="h-screen flex flex-col">
         <Head>
           <title>Carrinho</title>
           <meta name="description" content="Conferir e Finalizar o pedido" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main className="container flex flex-col items-center justify-center min-h-screen p-4 mx-auto">
+        <header className="mr-8">
           <Navbar data={data}/>
+        </header>
+        <main className="flex flex-1 flex-col items-center justify-center p-4 mx-auto">
           <ul className="grid grid-cols-1 gap-4">
             {
               Object.keys(shoppingCart).map((productName) => {
@@ -164,36 +186,53 @@ const Home: NextPage = () => {
               })
             }
           </ul>
-          <button
-            className="btn btn-outline btn-block my-4"
-            onClick={() => setShowCart(false)}
-          >Voltar</button>
-          <button
-            className="btn btn-outline btn-block"
-            onClick={handleCheckout}
-          >
-            Finalizar Compra
-            <div className="badge badge-secondary rounded-full">{
-              currencyFormatter(
-                Object.values(shoppingCart).reduce((acc, curr) => acc + curr.quantity * curr.price,0)
-              )
-            }</div>
-          </button>
         </main>
-      </>
+        <footer className="w-screen">
+            <button
+              className="btn btn-outline btn-block my-4"
+              onClick={() => setShowCart(false)}
+            >
+              <span className="text-xl text-bold">
+                Voltar
+              </span>
+            </button>
+            <button
+              className="btn btn-outline btn-block"
+              onClick={handleCheckout}
+            >
+              <span className="text-xl text-bold">
+                Finalizar Compra
+              </span>
+              <span className="badge badge-secondary rounded-full ml-2">
+                {
+                  currencyFormatter(
+                    Object.values(shoppingCart).reduce((acc, curr) => acc + curr.quantity * curr.price,0)
+                  )
+                }
+              </span>
+            </button>
+        </footer>
+      </div>
     )
   }
 
   return (
-    <>
+    <div className="h-screen flex flex-col">
       <Head>
         <title>Nova Venda</title>
         <meta name="description" content="Cadastrar uma nova venda no estoque" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container flex flex-col items-center justify-center min-h-screen p-4 mx-auto">
+      <header className="flex flex-col items-center justify-center">
         <Navbar data={data}/>
-        <ul className="grid gap-5 md:gap-5 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="btn-group pb-4">
+          <button className={`btn ${increment === 1 && 'btn-accent'}`} onClick={() => setIncrement(1)}>+1</button>
+          <button className={`btn ${increment === 2 && 'btn-accent'}`} onClick={() => setIncrement(2)}>+2</button>
+          <button className={`btn ${increment === 5 && 'btn-accent'}`} onClick={() => setIncrement(5)}>+5</button>
+        </div>
+      </header>
+      <main className="flex flex-1 overflow-auto items-center justify-center">
+        <ul className="mt-40 mb-10 grid gap-5 md:mt-0 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
           {query.data?.data.map(produto => {
             function handleAddToCart(id: string, name: string, price: number){
               return () => isAuthenticaded(data) && setShoppingCart((old) => {
@@ -212,23 +251,20 @@ const Home: NextPage = () => {
             </li>)
           })}
         </ul>
-        <div className="btn-group">
-          <button className={`btn ${increment === 1 && 'btn-accent'}`} onClick={() => setIncrement(1)}>+1</button>
-          <button className={`btn ${increment === 2 && 'btn-accent'}`} onClick={() => setIncrement(2)}>+2</button>
-          <button className={`btn ${increment === 5 && 'btn-accent'}`} onClick={() => setIncrement(5)}>+5</button>
-        </div>
-        <button
-          className="btn btn-outline btn-block"
-          disabled={!isAuthenticaded(data)}
-          onClick={() => setShowCart(true)}
-        >
-          Carrinho
-          <div className="badge badge-secondary rounded-full">+{
-            Object.values(shoppingCart).reduce((acc, curr) => acc + curr.quantity,0)
-          }</div>
-        </button>
       </main>
-    </>
+      <footer className="w-screen">
+          <button
+            className="btn btn-outline btn-block"
+            disabled={!isAuthenticaded(data)}
+            onClick={() => setShowCart(true)}
+          >
+            <span className="text-xl text-bold">
+              Carrinho
+            </span>
+            <CartCount />
+          </button>
+        </footer>
+    </div>
   );
 };
 
