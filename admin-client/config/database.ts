59822cc475d1842d9f76b5802da78bcc126f3599
Thales Menezes <thlmenezes@pm.me>
2022-10-08 02:49:33 -0300

    🗃️ replace sqlite with postgres

--- admin-client/config/database.ts
+++ admin-client/config/database.ts
@@ -2,10 +2,18 @@ import path from 'path';
 
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('PGHOST', 'localhost'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'zebstrika'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'postgres'),
+      schema: env('PGSCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('PGSSL_SELF', false),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
