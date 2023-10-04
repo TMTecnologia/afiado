c76bd7927d9c7986b0a89dfba733d2e913b2e258
Thales Menezes <thlmenezes.dev@gmail.com>
2023-10-04 02:02:11 -0300

    feat: revert add tanstack query

--- sales-client/.eslintrc.cjs
+++ sales-client/.eslintrc.cjs
@@ -4,12 +4,11 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint", "@tanstack/query"],
+  plugins: ["@typescript-eslint"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
--- sales-client/src/app/layout.tsx
+++ sales-client/src/app/layout.tsx
@@ -1,14 +1,13 @@
-import Providers from "~/lib/provider";
 import "~/styles/globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <Providers><body>{children}</body></Providers>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
