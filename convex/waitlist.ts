0e71b970f60a7338cd8ea3981c301d117e47e522
Thales Menezes <thlmenezes.dev@gmail.com>
2025-05-08 15:53:47 -0300

    feat(http): implement waitlist HTTP endpoint with CORS support

--- convex/waitlist.ts
+++ convex/waitlist.ts
@@ -1,7 +1,8 @@
+import { httpAction, internalMutation } from "./_generated/server";
+import { internal } from "./_generated/api";
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
 
-export const addEntry = mutation({
+export const addEmailToWaitlist = internalMutation({
   args: { email: v.string() },
   handler: async (ctx, args) => {
     const email = await ctx.db
@@ -9,12 +10,51 @@ export const addEntry = mutation({
       .withIndex("by_email", (q) => q.eq("email", args.email))
       .first();
 
-    // If the email is already in the waitlist, return it
+    // If the email is already in the waitlist, early return
     if (email) {
-      return email;
+      return;
     }
 
-    const newEntry = await ctx.db.insert("waitlist", { email: args.email });
-    return newEntry;
+    await ctx.db.insert("waitlist", { email: args.email });
   },
+});
+
+/**
+ * HTTP response status codes
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status}
+ */
+const HTTP_STATUS_CODE = {
+  CREATED: 201,
+  UNPROCESSABLE_CONTENT: 422,
+};
+
+export const addEmailToWaitlistHttp = httpAction(async (ctx, request) => {
+  const args = await request.json().catch(() => {
+    return new Response(
+      JSON.stringify({ message: "Invalid input: email required" }),
+      {
+        status: HTTP_STATUS_CODE.UNPROCESSABLE_CONTENT,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  });
+
+  // TODO: safe parse args here using zod to ensure e-mail
+  if (!("email" in args && typeof args.email === "string")) {
+    return new Response(
+      JSON.stringify({ message: "Invalid input: email required" }),
+      {
+        status: HTTP_STATUS_CODE.UNPROCESSABLE_CONTENT,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  await ctx.runMutation(internal.waitlist.addEmailToWaitlist, {
+    email: args.email,
+  });
+
+  return new Response(null, {
+    status: HTTP_STATUS_CODE.CREATED,
+  });
 });
